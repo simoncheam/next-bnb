e4e33fcb1f3d4aae1adb0dcf739fa8b05fcdc99a
Simon <81940817+simoncheam@users.noreply.github.com>
2024-10-29 18:09:01 -0400

    chore: save progress - implement favorite toggle button with authentication

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,9 +1,17 @@
 import { FaHeart } from 'react-icons/fa';
 import { Button } from '../ui/button';
+import { auth } from '@clerk/nextjs/server';
+import { CardSignInButton } from '../form/Buttons';
 
 const FavoriteToggleButton = ({ propertyId }: { propertyId: string }) => {
+  const { userId } = auth();
+  if (!userId) return <CardSignInButton />;
+
   return (
-    <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
+    <Button
+      size='icon'
+      variant='outline'
+      className='p-2 cursor-pointer'>
       <FaHeart />
     </Button>
   );
--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -5,6 +5,8 @@
 import { ReloadIcon } from '@radix-ui/react-icons';
 import { useFormStatus } from 'react-dom';
 import { Button } from '@/components/ui/button';
+import { SignInButton } from '@clerk/nextjs';
+import { FaRegHeart } from 'react-icons/fa';
 
 type btnSize = 'default' | 'lg' | 'sm';
 
@@ -17,7 +19,11 @@ type SubmitButtonProps = {
 export function SubmitButton({ className = '', text = 'submit', size = 'lg' }: SubmitButtonProps) {
   const { pending } = useFormStatus();
   return (
-    <Button type='submit' className={`capitalize ${className}`} disabled={pending} size={size}>
+    <Button
+      type='submit'
+      className={`capitalize ${className}`}
+      disabled={pending}
+      size={size}>
       {pending ? (
         <>
           <ReloadIcon className='mr-2 h-4 w-4 animate-spin' />
@@ -29,3 +35,18 @@ export function SubmitButton({ className = '', text = 'submit', size = 'lg' }: S
     </Button>
   );
 }
+
+export const CardSignInButton = () => {
+  return (
+    <SignInButton mode='modal'>
+      <Button
+        type='button'
+        size='icon'
+        variant='outline'
+        className='p-2 cursor-pointer'
+        asChild>
+        <FaRegHeart />
+      </Button>
+    </SignInButton>
+  );
+};

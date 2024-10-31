b008b18a5d91179b2b76294de8343a92d120341e
Simon <81940817+simoncheam@users.noreply.github.com>
2024-10-30 20:27:40 -0400

    chore: save progress - add ShareButton and FavoriteToggleButton to PropertyDetailsPage

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,3 +1,6 @@
+import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
+import BreadCrumbs from '@/components/properties/BreadCrumbs';
+import ShareButton from '@/components/properties/ShareButton';
 import { fetchPropertyDetails } from '@/utils/actions';
 import { redirect } from 'next/navigation';
 
@@ -10,6 +13,22 @@ const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
 
   const details = { baths, bedrooms, beds, guests };
 
-  return <div>PropertyDetailsPage</div>;
+  return (
+    <section>
+      <BreadCrumbs name={property.name} />
+      <header className='flex justify-between items-center mt-4'>
+        <h1 className='text-4xl font-bold capitalize'>{property.tagline}</h1>
+        <div className='flex items-center gap-x-4'>
+          {/* share button */}
+          <ShareButton
+            name={property.name}
+            propertyId={property.id}
+          />
+
+          <FavoriteToggleButton propertyId={params.id} />
+        </div>
+      </header>
+    </section>
+  );
 };
 export default PropertyDetailsPage;

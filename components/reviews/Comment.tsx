0ff6f103f42bdcc5058522f22b2acbd3020e7bd4
Simon <81940817+simoncheam@users.noreply.github.com>
2024-11-06 19:00:01 -0500

    chore: save progress - added review components and display functionality to property page

--- components/reviews/Comment.tsx
+++ components/reviews/Comment.tsx
@@ -1,6 +1,30 @@
-const Comment = () => {
+'use client';
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+
+const Comment = ({ comment }: { comment: string }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setIsExpanded((prev) => !prev);
+  };
+
+  const longComment = comment.length > 100;
+
+  const displayComment = longComment && !isExpanded ? `${comment.slice(0, 100)}...` : comment;
+
   return (
-    <div>Comment</div>
-  )
-}
-export default Comment
+    <div>
+      <p className='text-sm'>{displayComment}</p>
+      {longComment && (
+        <Button
+          variant='link'
+          className='pl-0 text-muted-foreground'
+          onClick={toggleExpanded}>
+          {isExpanded ? 'Show less' : 'Show more'}
+        </Button>
+      )}
+    </div>
+  );
+};
+export default Comment;
--- components/reviews/PropertyReviews.tsx
+++ components/reviews/PropertyReviews.tsx
@@ -1,6 +1,36 @@
-const PropertyReviews = () => {
+import { fetchPropertyReviews } from '@/utils/actions';
+import Title from '@/components/properties/Title';
+import ReviewCard from './ReviewCard';
+
+const PropertyReviews = async ({ propertyId }: { propertyId: string }) => {
+  const reviews = await fetchPropertyReviews(propertyId);
+
+  if (reviews.length < 1) return null;
+
   return (
-    <div>PropertyReviews</div>
-  )
-}
-export default PropertyReviews
+    <div className='mt-8'>
+      <Title text='Reviews' />
+      <div className='grid md:grid-cols-2 gap-8 mt-4'>
+        {reviews.map((review) => {
+          const { comment, rating } = review;
+          const { firstName, profileImage } = review.profile;
+
+          const reviewInfo = {
+            comment,
+            rating,
+            name: firstName,
+            image: profileImage,
+          };
+
+          return (
+            <ReviewCard
+              key={review.id}
+              reviewInfo={reviewInfo}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default PropertyReviews;
